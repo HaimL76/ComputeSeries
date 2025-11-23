a5544a76a79e51263941a0a03f3b64e1cf3c0074
unknown <haiml76@yahoo.com>
2025-11-24 00:02:00 +0200

    231

--- process_file.py
+++ process_file.py
@@ -205,6 +205,8 @@ class ProcessFolder:
                     if series_product_counter in dict_random_numbers:
                         list_debug_data.append(str_debug)
 
+                fw_sage_series_sums.write(f"print(f\"{var_h}={{{var_h}}} #### {str_case_indices} [substitution:{str_substitution}]\")\n")
+
                 fw_sage_series_sums.write(f"print(f\"{var_h}={{{var_h}}} #### {str_case_indices} [converted polynomial={str_converted_polynomial}]\")\n")
 
                 fw_sage_series_sums.write(f"f += {var_h}\n")
