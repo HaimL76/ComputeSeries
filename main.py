912335a2a0cbc37c2053af7dd6a7a2c446ed2278
Haim Lavi <HaimL1@verifone.com>
2025-11-25 21:55:34 +0200

    247

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from substitution import VariableSubstitution
 const_coefficient: str = "(1-p^{-1})"
 coeff: str = const_coefficient
 
-def create_symmetry_factors_program():
+def create_symmetry_factors_program(level: int = 1):
     with open(".\\saved_output\\intermediates.txt", "r") as fr:
         with open(".\\saved_output\\compute_symmetry_factors.txt", "w") as fw:
             fw.write("# Define the polynomial ring\n")
@@ -21,18 +21,43 @@ def create_symmetry_factors_program():
             fw.write("F = R.fraction_field()\n")
             fw.write("psi = F.hom([1/p, 1/t], F)\n")
 
+            hs: dict[str, list] = {}
+
             for line in fr:
                 if line.startswith("h_"):
                     arr: list[str] = line.split("=")
                     if isinstance(arr, list) and len(arr) > 1:
                         var_name = arr[0]
-                        sf_var_name = f"sf_{var_name}"
-                        fw.write(f"{line}\n")
-                        fw.write(f"{sf_var_name}=psi({var_name})/{var_name}\n")
-                        fw.write(f"print(f\"{sf_var_name}={{{sf_var_name}}}\")\n")
-                        print(var_name)
-                    else:
-                        _ = 0
+
+                        arr_indices: list[int] = [int(str_index) for str_index in var_name.split("_") if str_index.isnumeric()]
+
+                        list_indices: list[int] = []
+
+                        for i in range(level):
+                            if i < len(arr_indices):
+                                list_indices.append(arr_indices[i])
+
+                        str_indices: str = "_".join([str(i) for i in list_indices])
+
+                        if str_indices not in hs.keys():
+                            hs[str_indices] = []
+
+                        hs[str_indices].append(var_name)
+
+            for str_indices in hs.keys():
+                list_vars: list[str] = hs[str_indices]
+
+                sf_var_name = f"sf_{str_indices}"
+
+                var_h = f"h_{str_indices}"
+
+                fw.write(f"{var_h} = QQ.zero()\n")
+
+                for str_var in list_vars:
+                    fw.write(f"{var_h}+={str_var}\n")
+
+            fw.write(f"{sf_var_name}=psi({var_h})/{var_h}\n")
+            fw.write(f"print(f\"{sf_var_name}={{{sf_var_name}}}\")\n")
 
 def main():
     create_symmetry_factors_program()
