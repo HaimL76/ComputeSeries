a5183429b4c8906013bb01d190f1c1f7339ef899
Haim Lavi <HaimL1@verifone.com>
2026-01-17 22:52:37 +0200

    348

--- list_orders.py
+++ list_orders.py
@@ -19,5 +19,55 @@ def list_orders(file_path: str) -> list[int]:
             arr = [s.strip() for s in arr if s.startswith("v_")]
 
             if isinstance(arr, list) and len(arr) > 3:
-                print(line)
+                index: int = 0
+
+                list_vars: list[list[int]] = []
+                list_rels: list[bool] = []
+
+                while line:
+                    if re.search(r'^v_[1234]', line):
+                        list_index: list[int] = []
+
+                        str_var: str = line[0:3]
+
+                        str_num: str = str_var[2]
+
+                        if str_num and str_num.isnumeric:
+                            list_index.append(int(str_num))
+
+                        line = line[3:]
+
+                        if line[0] == "+":
+                            line = line[1:]
+                            str_var: str = line[0:3]
+
+                            str_num: str = str_var[2]
+
+                            if str_num and str_num.isnumeric:
+                                list_index.append(int(str_num))
+                            
+                            line = line[3:]
+
+                        if len(list_index) > 0:
+                            list_vars.append(list_index)
+                    elif line.startswith(">"):
+                        line = line[1:]
+                        list_rels.append(True)
+                    elif line.startswith(r"\geq"):
+                        line = line[len(r"\geq"):]
+                        list_rels.append(False)
+                    else:
+                        line = line[1:]
+
+                list_strs: list[str] = []
+
+                for i in range(len(list_vars)):
+                    list_index = list_vars[i]
+                    str_vars: str = "+".join([f"v_{index}" for index in list_index])
+
+                    list_strs.append(str_vars)
+
+                    list_strs.append(">" if list_rels[i] else ">=")
+
+                print(" ".join(list_strs))
     return orders
